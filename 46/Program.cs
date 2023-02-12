ad0954a56d7fc8a8e51491dc1d1040703588a82f
Nikolayz <zans55@yandex.ru>
2023-02-12 19:54:31 +0300

    46

---
+++ 46/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Создать массив из 123 элементов. Заполнить 
+случайными положительными числами от 0 до 1000. 
+Найти количество элементов которые принадлежат отрезку [10,99].*/
+int[] arr;
+arr=new int[122];
+Random random=new Random();
+for(int i=0;i<arr.Length;i++)
+arr[i]=random.Next(0,1000);
+
+for(int i=0;i<arr.Length;i++)
+if (arr[i]>10 && arr[i]<99)
+System.Console.WriteLine(arr[i]);
+
+int s=0;
+for(int i=0;i<arr.Length;i++)
+   if (arr[i]>10 && arr[i]<99)
+  {s=s+1;
+  }
+
+System.Console.WriteLine($"Количество элементов' = {s}");
+
